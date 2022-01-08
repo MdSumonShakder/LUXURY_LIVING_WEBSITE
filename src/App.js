b88d85e7a4051509c4adae2b1ce395e783803354
mdsumon <mdsumonshakder96@gmail.com>
2022-01-08 23:50:28 +0600

    deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Page/Home/Home/Home";
 import NotFound from "./Page/NotFound/NotFound";
-import "./App.css";
 import Navigation from "./Page/Navigation/Navigation";
 import Login from "./Page/Login/Login/Login";
 import Register from "./Page/Login/Register/Register";
--- src/Page/Login/Register/Register.js
+++ src/Page/Login/Register/Register.js
@@ -5,9 +5,9 @@ import useAuth from "./../../../hooks/useAuth";
 import Zoom from "react-reveal/Zoom";
 
 const Register = () => {
+  const { user, registerUser, loading, error } = useAuth();
   const [loginData, setLoginData] = useState({});
   const navigate = useNavigate();
-  const { user, registerUser, loading, error } = useAuth();
 
   const handleOnBlur = (e) => {
     const field = e.target.name;
@@ -18,7 +18,7 @@ const Register = () => {
   };
   const handleLoginSubmit = (e) => {
     if (loginData.password !== loginData.password2) {
-      alert("Your Password did not Match!");
+      <Alert>Your Password did not Match!</Alert>;
       return;
     }
 
@@ -26,103 +26,100 @@ const Register = () => {
     e.preventDefault();
   };
   return (
-    <div
-      className=" pt-5 pb-5 text-center"
-      style={{
-        marginTop: "50px",
-        width: "100%",
-        backgroundImage: `url('https://images.pexels.com/photos/792115/pexels-photo-792115.jpeg?cs=srgb&dl=pexels-egor-kamelev-792115.jpg&fm=jpg')`,
-        backgroundPasition: "center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div class="container">
-        <Zoom top>
-          <h4 className="p-3 text-white">Please Register</h4>
-        </Zoom>
-        <Zoom>
-          {!loading && (
-            <form onSubmit={handleLoginSubmit} className="contact-forms">
-              <div class="col-md-6 d-block mx-auto">
-                <input
-                  type="text"
-                  name="name"
-                  id=""
-                  onBlur={handleOnBlur}
-                  placeholder="Full Name"
-                />
-              </div>
-              <div class="col-md-6 d-block mx-auto">
-                <input
-                  type="email"
-                  name="email"
-                  id=""
-                  onBlur={handleOnBlur}
-                  placeholder="Email Address"
-                />
-              </div>
-              <div class="col-md-6 d-block mx-auto">
-                <input
-                  type="number"
-                  name="number"
-                  id=""
-                  onBlur={handleOnBlur}
-                  placeholder="Phone Number"
-                />
-              </div>
-              <div class="col-md-6 d-block mx-auto">
-                <input
-                  type="password"
-                  name="password"
-                  onBlur={handleOnBlur}
-                  id=""
-                  placeholder="Password"
-                />
-              </div>
-              <div class="col-md-6 d-block mx-auto">
-                <input
-                  type="password"
-                  name="password2"
-                  id=""
-                  onBlur={handleOnBlur}
-                  placeholder="Confirm Password"
-                />
-              </div>
-              <div class="col-md-6 d-block mx-auto">
-                <div className="bannerText">
-                  <button className="mb-3 fw-bolder" type="submit">
-                    REGISTER
-                  </button>
+    <>
+      <div
+        className=" pt-5 pb-5 text-center"
+        style={{
+          marginTop: "50px",
+          width: "100%",
+          backgroundImage: `url('https://images.pexels.com/photos/792115/pexels-photo-792115.jpeg?cs=srgb&dl=pexels-egor-kamelev-792115.jpg&fm=jpg')`,
+          backgroundPasition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="container">
+          <Zoom top>
+            <h4 className="p-3">Please Register</h4>
+          </Zoom>
+          <Zoom>
+            {!loading && (
+              <form onSubmit={handleLoginSubmit} className="contact-forms">
+                <div className="col-md-6 d-block mx-auto">
+                  <input
+                    type="text"
+                    name="name"
+                    onBlur={handleOnBlur}
+                    placeholder="Full Name"
+                  />
                 </div>
-              </div>
-              <NavLink to="/login">
-                <Button className="mb-3 fw-bolder bg-dark text-white border-0">
-                  ALREADY REGISTERED ? PLEASE LOGIN
-                </Button>
-              </NavLink>
-            </form>
-          )}
-        </Zoom>
-        {loading && <Spinner animation="border" variant="danger" />}
+                <div className="col-md-6 d-block mx-auto">
+                  <input
+                    type="email"
+                    name="email"
+                    onBlur={handleOnBlur}
+                    placeholder="Email Address"
+                  />
+                </div>
+                <div className="col-md-6 d-block mx-auto">
+                  <input
+                    type="number"
+                    name="number"
+                    onBlur={handleOnBlur}
+                    placeholder="Phone Number"
+                  />
+                </div>
+                <div className="col-md-6 d-block mx-auto">
+                  <input
+                    type="password"
+                    name="password"
+                    onBlur={handleOnBlur}
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="col-md-6 d-block mx-auto">
+                  <input
+                    type="password"
+                    name="password2"
+                    onBlur={handleOnBlur}
+                    placeholder="Confirm Password"
+                  />
+                </div>
+                <div className="col-md-6 d-block mx-auto">
+                  <div className="bannerText">
+                    <button className="mb-3 fw-bolder" type="submit">
+                      REGISTER
+                    </button>
+                  </div>
+                </div>
+                <NavLink to="/login">
+                  <Button className="mb-3 fw-bolder bg-dark text-white border-0">
+                    ALREADY REGISTERED ? PLEASE LOGIN
+                  </Button>
+                </NavLink>
+              </form>
+            )}
+          </Zoom>
+          {loading && <Spinner animation="border" variant="danger" />}
 
-        {user?.email && (
-          <Alert
-            className=" bg-white w-25 d-block mx-auto fw-bolder"
-            variant="success"
-          >
-            Register Successfully!
-          </Alert>
-        )}
-        {error && (
-          <Alert
-            className="bg-warning w-25 d-block mx-auto fw-bolder"
-            variant="dark"
-          >
-            Firebase-Error:(Email-already-in-use !!! Please Login...)
-          </Alert>
-        )}
+          {user?.email && (
+            <Alert
+              className=" bg-white w-25 d-block mx-auto fw-bolder"
+              variant="success"
+            >
+              Register Successfully!
+            </Alert>
+          )}
+          {error && (
+            <Alert
+              className="bg-warning w-25 d-block mx-auto fw-bolder"
+              variant="dark"
+            >
+              Firebase-Error:(Email-already-in-use !!! Please Login...)
+            </Alert>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
